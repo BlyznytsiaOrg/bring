6156379fc3d6a0e724d4bf0df32d2d6a676e41b9
levik <zhenya12@gmail.com>
2023-11-10 18:12:36 +0200

    remove demo classes.

--- core/src/main/java/com/bobocode/bring/core/demo/Drink.java
+++
@@ -1,5 +0,0 @@
-package com.bobocode.bring.core.demo;
-
-public interface Drink {
-    String make();
-}
--- core/src/main/java/com/bobocode/bring/core/demo/Espresso.java
+++
@@ -1,9 +0,0 @@
-package com.bobocode.bring.core.demo;
-
-//@Component
-public class Espresso implements Drink {
-    @Override
-    public String make() {
-        return "Brewing a strong espresso!";
-    }
-}
