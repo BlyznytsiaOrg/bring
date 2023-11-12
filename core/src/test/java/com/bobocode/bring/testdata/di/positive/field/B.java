6c9c55d2b1f1dfcef27fafadfa228b250555ce20
levik <zhenya12@gmail.com>
2023-11-12 22:13:33 +0200

    add unit test to cover constructor, setter and field injection.

---
+++ core/src/test/java/com/bobocode/bring/testdata/di/positive/field/B.java
@@ -0,0 +1,17 @@
+package com.bobocode.bring.testdata.di.positive.field;
+
+import com.bobocode.bring.core.anotation.Autowired;
+import com.bobocode.bring.core.anotation.Component;
+import lombok.ToString;
+
+@ToString
+@Component
+public class B {
+
+    @Autowired
+    private A a;
+
+    public A getA() {
+        return a;
+    }
+}
