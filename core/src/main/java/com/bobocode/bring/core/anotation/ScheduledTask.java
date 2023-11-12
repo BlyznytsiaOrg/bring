5cfe82019c24a86e5dbe34c983a00f09f64d7a3c
levik <zhenya12@gmail.com>
2023-11-12 23:00:49 +0200

    add java doc for Component, Autowired, ScheduledTask, Service.

--- core/src/main/java/com/bobocode/bring/core/anotation/ScheduledTask.java
+++ core/src/main/java/com/bobocode/bring/core/anotation/ScheduledTask.java
@@ -6,11 +6,59 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * Annotation used to mark a method as a scheduled task. This annotation is part of the Bring Framework's scheduling
+ * support, allowing the method to be automatically invoked at specified intervals.
+ *
+ * <p>The annotated method must adhere to the requirements for a scheduled task method, including having no parameters
+ * and a void return type.
+ *
+ * <p>You can customize the scheduling behavior by providing values for the {@code value}, {@code initialDelay},
+ * {@code period}, and {@code timeUnit} attributes. If not specified, the default values will be used.
+ *
+ *
+ * <p><strong>Usage Example:</strong>
+ * <pre>
+ * {@code
+ *  @Service
+ *  public class MyScheduledService {
+ *
+ *      @ScheduledTask(value = "myTask", initialDelay = 1000, period = 5000, timeUnit = TimeUnit.MILLISECONDS)
+ *      public void myScheduledMethod() {
+ *          // Task logic
+ *      }
+ *  }
+ * }
+ * </pre>
+ *
+ *  @author Blyzhnytsia Team
+ *  @since 1.0
+ */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ScheduledTask {
+    /**
+     * The name of the scheduled task. If not specified, the method name will be used as the task name.
+     *
+     * @return the name of the scheduled task
+     */
     String value() default "";
-    long initialDelay() default 0;
-    long period();
-    TimeUnit timeUnit() default TimeUnit.SECONDS;
+    /**
+     * The initial delay before the first execution of the task, in milliseconds.
+     *
+     * @return the initial delay in milliseconds
+     */
+    long initialDelay() default 1000;
+    /**
+     * The fixed rate between consecutive executions of the task, in milliseconds.
+     *
+     * @return the period between executions in milliseconds
+     */
+    long period() default 5000;
+    /**
+     * The time unit for the initial delay and period attributes. Defaults to {@code TimeUnit.MILLISECONDS}.
+     *
+     * @return the time unit for the initial delay and period
+     */
+    TimeUnit timeUnit() default TimeUnit.MILLISECONDS;
 }
--- core/src/main/java/com/bobocode/bring/core/anotation/Service.java
+++ core/src/main/java/com/bobocode/bring/core/anotation/Service.java
@@ -5,8 +5,44 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+
+/**
+ * Indicates that an annotated class is a Bring component. This annotation serves as a marker for Bring to
+ * automatically detect and register the annotated class as a bean during component scanning.
+ *
+ * <p>By default, the name of the bean will be the simple name of the annotated class. You can customize the bean name
+ * by providing a value to the {@code value} attribute. If no value is provided, the bean name will be generated based
+ * on the class name with the initial letter in lowercase.
+ *
+ * <p>This annotation is part of the Bring Framework's component scanning mechanism, which allows for automatic
+ * discovery and registration of Bring beans.
+ *
+ * <p><strong>Usage Example:</strong>
+ * <pre>
+ * {@code
+ *  @Service
+ *  public class MyComponent {
+ *      // Class definition
+ *  }
+ *
+ *  // Custom bean name
+ *  @Service("customBeanName")
+ *  public class AnotherComponent {
+ *      // Class definition
+ *  }
+ * }
+ * </pre>
+ *
+ *  @author Blyzhnytsia Team
+ *  @since 1.0
+ */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Service {
+    /**
+     * The name of the Bring bean. If not specified, the bean name will be generated based on the class name.
+     *
+     * @return the name of the Spring bean
+     */
     String value() default "";
 }
