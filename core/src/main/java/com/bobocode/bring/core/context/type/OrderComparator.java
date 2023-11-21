ab1983c6885435d88df0904e621516f5fe009709
Andrew Pikozh <pikozh13@gmail.com>
2023-11-22 00:24:45 +0200

    BLZT-27 refactoring

--- core/src/main/java/com/bobocode/bring/core/context/type/OrderComparator.java
+++ core/src/main/java/com/bobocode/bring/core/context/type/OrderComparator.java
@@ -6,24 +6,18 @@ import java.util.Comparator;
 import java.util.Objects;
 
 /**
- * {@link Comparator} implementation for objects marked {@link Order} , sorting
+ * {@link Comparator} implementation for objects marked {@link Order}, sorting
  * by order value ascending,
  */
 public class OrderComparator implements Comparator<Class<?>> {
     @Override
     public int compare(Class<?> o1, Class<?> o2) {
-        if (!Objects.isNull(o1.getAnnotation(Order.class))) {
-            if (Objects.isNull(o2.getAnnotation(Order.class))) {
-                return -1;
-            } else {
-                return Integer.compare(o1.getAnnotation(Order.class).value(), o2.getAnnotation(Order.class).value());
-            }
-        } else {
-            if (!Objects.isNull(o2.getAnnotation(Order.class))) {
-                return 1;
-            } else {
-                return 0;
-            }
-        }
+        return Comparator.comparingInt(this::getOrderValue)
+                .compare(o1, o2);
+    }
+
+    private int getOrderValue(Class<?> clazz) {
+        Order order = clazz.getAnnotation(Order.class);
+        return Objects.nonNull(order) ? order.value() : Integer.MAX_VALUE;
     }
 }
