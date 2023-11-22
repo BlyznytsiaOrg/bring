8823bba04a208548c87ac04fb69f32376fa91a4c
ivan <ivan95kos@gmail.com>
2023-11-22 21:31:45 +0100

    [BLZT-42] add controller for processing static files

---
+++ web/src/main/java/com/bobocode/bring/web/servlet/StaticResourceController.java
@@ -0,0 +1,60 @@
+package com.bobocode.bring.web.servlet;
+
+import com.bobocode.bring.core.anotation.RestController;
+import com.bobocode.bring.web.service.StaticResourceService;
+import com.bobocode.bring.web.servlet.annotation.GetMapping;
+import com.bobocode.bring.web.servlet.annotation.RequestMapping;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+/**
+ * RestController responsible for handling static resource requests.
+ * Provides an endpoint to retrieve static files based on the requested URI.
+ *
+ * @author Blyzhnytsia Team
+ * @version 1.0
+ * @since 2023-11-22
+ */
+@RestController
+@RequestMapping
+@RequiredArgsConstructor
+public class StaticResourceController implements BringServlet {
+
+    // Service for retrieving static resources
+    private final StaticResourceService staticResourceService;
+
+    /**
+     * Handles GET requests for static resources.
+     *
+     * @param request  The incoming HTTP request.
+     * @param response The outgoing HTTP response.
+     */
+    @GetMapping(path = "/static/*")
+    @SneakyThrows
+    public void getStaticFile(HttpServletRequest request, HttpServletResponse response) {
+        Path resourceAbsolutePath = staticResourceService.getStaticFile(request.getRequestURI());
+
+        setContentType(response, resourceAbsolutePath);
+        Files.copy(resourceAbsolutePath, response.getOutputStream());
+    }
+
+    /**
+     * Sets the content type in the HTTP response based on the file type.
+     *
+     * @param response The outgoing HTTP response.
+     * @param path     The path of the static file.
+     * @throws IOException If an I/O error occurs while processing the response.
+     */
+    private void setContentType(HttpServletResponse response, Path path) throws IOException {
+        String contentType = Files.probeContentType(path);
+        if (contentType != null) {
+            response.setContentType(contentType);
+        }
+    }
+}
--- web/src/main/java/com/bobocode/bring/web/servlet/http/MediaType.java
+++ web/src/main/java/com/bobocode/bring/web/servlet/http/MediaType.java
@@ -2,10 +2,31 @@ package com.bobocode.bring.web.servlet.http;
 
 import lombok.experimental.UtilityClass;
 
+/**
+ * Utility class for representing common media types used in HTTP requests and responses.
+ * Provides constants for commonly used media type values.
+ *
+ * <p>This class is marked as a utility class with the {@link lombok.experimental.UtilityClass} annotation,
+ * indicating that it contains only static methods or fields and should not be instantiated.
+ *
+ * @author Blyzhnytsia Team
+ * @version 1.0
+ * @since 2023-11-17
+ */
 @UtilityClass
 public class MediaType {
 
+    /**
+     * Represents the "application/json" media type.
+     */
     public static final String APPLICATION_JSON_VALUE = "application/json";
+    /**
+     * Represents the "text/html" media type.
+     */
+    public static final String TEXT_HTML_VALUE = "text/html";
+    /**
+     * Represents the "UTF-8" character encoding.
+     */
     public static final String UTF8_VALUE = "UTF-8";
 
 }
