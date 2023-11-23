5afa637c9685a052af06265bf050fd2878bc2a36
levik <zhenya12@gmail.com>
2023-11-23 14:24:16 +0200

    BLZT-43: add java doc for core main component.

--- core/src/main/java/com/bobocode/bring/core/env/BringSourceLoader.java
+++ core/src/main/java/com/bobocode/bring/core/env/BringSourceLoader.java
@@ -2,9 +2,27 @@ package com.bobocode.bring.core.env;
 
 import java.util.Map;
 
+/**
+ * Interface for a Bring source loader, defining methods to retrieve file extensions and load resources.
+ *
+ *  @author Blyzhnytsia Team
+ *  @since 1.0
+ */
+
 public interface BringSourceLoader {
 
+    /**
+     * Retrieves the file extensions supported by this source loader.
+     *
+     * @return A string representing the file extensions supported by this loader.
+     */
     String getFileExtensions();
 
+    /**
+     * Loads a resource by the given name.
+     *
+     * @param name The name of the resource to be loaded.
+     * @return A map containing data loaded from the specified resource.
+     */
     Map<String, String> load(String name);
 }
--- core/src/main/java/com/bobocode/bring/core/env/impl/BringPropertySourceLoader.java
+++ core/src/main/java/com/bobocode/bring/core/env/impl/BringPropertySourceLoader.java
@@ -12,16 +12,35 @@ import java.util.Properties;
 
 import static java.util.stream.Collectors.toMap;
 
+
+/**
+ * Implementation of BringSourceLoader for loading properties from files.
+ *
+ *  @author Blyzhnytsia Team
+ *  @since 1.0
+ */
 @Slf4j
 public class BringPropertySourceLoader implements BringSourceLoader {
 
     public static final String APPLICATION_PROPERTIES = "application.properties";
 
+    /**
+     * Returns the file extension handled by this loader.
+     *
+     * @return The file extension, which is "properties".
+     */
     @Override
     public String getFileExtensions() {
         return "properties";
     }
 
+    /**
+     * Loads properties from the default file ("application.properties") and optionally from another specified file,
+     * merging them where the latter overrides the former.
+     *
+     * @param name The name of the file containing additional properties. If null, defaults to "application.properties".
+     * @return A map containing the merged properties from both sources.
+     */
     @Override
     public Map<String, String> load(String name) {
         Map<String, String> defaultProperties = loadProperties(APPLICATION_PROPERTIES);
@@ -31,6 +50,12 @@ public class BringPropertySourceLoader implements BringSourceLoader {
     }
 
 
+    /**
+     * Loads properties from a specified file.
+     *
+     * @param fileName The name of the file to load properties from. If null, defaults to "application.properties".
+     * @return A map containing the properties loaded from the specified file.
+     */
     private static Map<String, String> loadProperties(String fileName) {
         String defaultFileName  = (fileName == null) ? APPLICATION_PROPERTIES : fileName;
         log.debug("Load Property file {}", defaultFileName);
