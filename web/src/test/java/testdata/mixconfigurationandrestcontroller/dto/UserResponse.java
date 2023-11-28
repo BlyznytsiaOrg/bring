dd90445e8d6024eee3abaf1675176509e75b6a11
PolukovY <73576438+PolukovY@users.noreply.github.com>
2023-11-28 13:53:31 +0200

    Mix configuration and component and RestController and Configuration (#80)
    * mix configuration and service
    
    * add missing Bean
    
    * BLZT-43: fix injection for web and add tests.
    
    ---------
    
    Co-authored-by: levik <zhenya12@gmail.com>

---
+++ web/src/test/java/testdata/mixconfigurationandrestcontroller/dto/UserResponse.java
@@ -0,0 +1,11 @@
+package testdata.mixconfigurationandrestcontroller.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+public class UserResponse {
+
+    private String shortenedUrl;
+}
