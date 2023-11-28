effbe2ccbade4b6342d52583cbc355fedf469c89
levik <zhenya12@gmail.com>
2023-11-28 09:44:12 +0200

    add missing Bean

--- core/src/test/java/testdata/di/positive/mixconfigurationandservice/MyConfiguration.java
+++ core/src/test/java/testdata/di/positive/mixconfigurationandservice/MyConfiguration.java
@@ -1,10 +1,12 @@
 package testdata.di.positive.mixconfigurationandservice;
 
+import com.bobocode.bring.core.annotation.Bean;
 import com.bobocode.bring.core.annotation.Configuration;
 
 @Configuration
 public class MyConfiguration {
 
+    @Bean
     public BeanA beanA() {
         return new BeanA();
     }
