c5334aaaad5f8082c5d6d86a3b6fbeef832c38d2
PolukovY <73576438+PolukovY@users.noreply.github.com>
2023-11-30 17:12:31 +0200

    BLZT-43: add javadoc and some unittests. (#97)
    * BLZT-43: add javadoc and some unittests.
    
    * BLZT-43: add javadoc.
    
    * BLZT-43: add javadoc.
    
    ---------
    
    Co-authored-by: levik <zhenya12@gmail.com>

--- core/src/main/java/com/bobocode/bring/core/domain/BeanDefinition.java
+++ core/src/main/java/com/bobocode/bring/core/domain/BeanDefinition.java
@@ -6,6 +6,12 @@ import lombok.Getter;
 import java.lang.reflect.Method;
 import java.util.Objects;
 
+/**
+ * Represents the definition of a bean within an application context.
+ *
+ * @author Blyzhnytsia Team
+ * @since 1.0
+ */
 @Getter
 @Builder
 public class BeanDefinition {
@@ -24,18 +30,38 @@ public class BeanDefinition {
 
     private boolean isPrimary;
 
+    /**
+     * Checks if the bean is of type Configuration.
+     *
+     * @return True if the bean is of type Configuration, otherwise false
+     */
     public boolean isConfiguration() {
         return this.beanType == BeanTypeEnum.CONFIGURATION;
     }
 
+    /**
+     * Checks if the bean is a configuration bean.
+     *
+     * @return True if the bean is a configuration bean, otherwise false
+     */
     public boolean isConfigurationBean() {
         return Objects.nonNull(method);
     }
 
+    /**
+     * Checks if the bean's scope is set to Prototype.
+     *
+     * @return True if the bean's scope is Prototype, otherwise false
+     */
     public boolean isPrototype() {
         return scope == BeanScope.PROTOTYPE;
     }
 
+    /**
+     * Checks if the bean is configured for proxying.
+     *
+     * @return True if the bean is configured for proxying, otherwise false
+     */
     public boolean isProxy() {
         return proxyMode == ProxyMode.ON;
     }
--- core/src/main/java/com/bobocode/bring/core/utils/Pair.java
+++ core/src/main/java/com/bobocode/bring/core/utils/Pair.java
@@ -3,14 +3,38 @@ package com.bobocode.bring.core.utils;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+/**
+ * A simple generic class representing a pair of two values: left and right.
+ *
+ * @param <L> The type of the left value
+ * @param <R> The type of the right value
+ *
+ * @author Blyzhnytsia Team
+ * @since 1.0
+ */
 @AllArgsConstructor
 @Data
 public class Pair<L, R> {
 
+    /**
+     * The left value of the pair.
+     */
     L left;
-  
+
+    /**
+     * The right value of the pair.
+     */
     R right;
-  
+
+    /**
+     * Creates a new Pair object with the specified left and right values.
+     *
+     * @param <L>   The type of the left value
+     * @param <R>   The type of the right value
+     * @param left  The left value
+     * @param right The right value
+     * @return A new Pair object with the given left and right values
+     */
     public static <L, R> Pair<L, R> of(L left, R right) {
       return new Pair<>(left, right);
     }
