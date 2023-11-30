c476996bc52feea139e3cd2a49cb1e93b5d4fa03
Andrew Pikozh <pikozh13@gmail.com>
2023-11-30 19:53:20 +0200

    BLZT-71 add javadoc

--- core/src/main/java/com/bobocode/bring/core/exception/PostConstructException.java
+++ core/src/main/java/com/bobocode/bring/core/exception/PostConstructException.java
@@ -1,7 +1,20 @@
 package com.bobocode.bring.core.exception;
 
+/**
+ * Exception thrown when method annotated with @PostConstruct violates rules of usage such as have parameters.
+ * Extends RuntimeException.
+ *
+ * @author Blyzhnytsia Team
+ * @since 1.0
+ */
 public class PostConstructException extends RuntimeException {
-    public static String POST_CONSTRUCT_EXCEPTION_MESSAGE ="@PostConstruct should be added to method without parameters";
+    public static final String POST_CONSTRUCT_EXCEPTION_MESSAGE = "@PostConstruct should be added to method without parameters";
+
+    /**
+     * Constructs a PostConstructException with the specified message.
+     *
+     * @param cause The Throwable cause for this exception
+     */
     public PostConstructException(Throwable cause) {
         super(POST_CONSTRUCT_EXCEPTION_MESSAGE, cause);
     }
