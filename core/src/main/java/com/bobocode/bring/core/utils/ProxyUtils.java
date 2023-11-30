164938720a1bb7352ef7147e03ce8c666bbc9b36
levik <zhenya12@gmail.com>
2023-11-30 15:14:42 +0200

    BLZT-43: add javadoc and some unittests.

--- core/src/main/java/com/bobocode/bring/core/utils/ProxyUtils.java
+++ core/src/main/java/com/bobocode/bring/core/utils/ProxyUtils.java
@@ -9,9 +9,23 @@ import lombok.experimental.UtilityClass;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 
+/**
+ * Utility class providing methods to create proxies.
+ *
+ * @author Blyzhnytsia Team
+ * @since 1.0
+ */
 @UtilityClass
 public class ProxyUtils {
 
+    /**
+     * Creates a proxy instance for the given class using a specified constructor and arguments.
+     *
+     * @param clazz       The class to create a proxy for
+     * @param constructor The constructor to be used for proxy creation
+     * @param args        The arguments to pass to the constructor
+     * @return The proxy object for the specified class
+     */
     @SneakyThrows
     public Object createProxy(Class<?> clazz, Constructor<?> constructor, Object[] args) {
         ProxyFactory proxyFactory = new ProxyFactory();
@@ -22,6 +36,9 @@ public class ProxyUtils {
         return proxyFactory.create(new Class<?>[0], new Object[0], methodHandler);
     }
 
+    /**
+     * Handler class implementing MethodHandler for proxy invocation.
+     */
     @RequiredArgsConstructor
     public class Handler implements MethodHandler {
         
@@ -29,6 +46,16 @@ public class ProxyUtils {
         
         private final Object[] constructorArgs;
 
+        /**
+         * Invokes the method on the proxy object and delegates to the actual object.
+         *
+         * @param self     The proxy object
+         * @param thisMethod The method being invoked on the proxy
+         * @param proceed  The proceed method
+         * @param args     The arguments for the method invocation
+         * @return The result of the method invocation on the actual object
+         * @throws Throwable If an error occurs during method invocation
+         */
         @Override
         public Object invoke(Object self, Method thisMethod, Method proceed, Object[] args) throws Throwable {
             Object obj = constructor.newInstance(constructorArgs);
--- core/src/main/java/com/bobocode/bring/core/utils/ReflectionUtils.java
+++ core/src/main/java/com/bobocode/bring/core/utils/ReflectionUtils.java
@@ -20,6 +20,14 @@ import java.util.*;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
+
+/**
+ * Utility class providing reflection-related functionalities, including methods for constructor instantiation,
+ * field manipulation, method invocation, and annotation processing.
+ *
+ * @author Blyzhnytsia Team
+ * @since 1.0
+ */
 @UtilityClass
 @Slf4j
 public final class ReflectionUtils {
@@ -30,7 +38,7 @@ public final class ReflectionUtils {
             BEAN_SHOULD_HAVE_MESSAGE + "constructor with one parameter '%s'.";
     private static final String BEAN_SHOULD_HAVE_CONSTRUCTOR_WITH_PARAMETERS_MESSAGE =
             BEAN_SHOULD_HAVE_MESSAGE + "constructor with parameters '%s'.";
-    public static final String DELIMITER = ", ";
+    private static final String DELIMITER = ", ";
 
     public static final OrderComparator ORDER_COMPARATOR = new OrderComparator();
     private static final Paranamer info = new CachingParanamer(new QualifierAnnotationParanamer(new BytecodeReadingParanamer()));
@@ -38,10 +46,23 @@ public final class ReflectionUtils {
 
     private static final String SET_METHOD_START_PREFIX = "set";
 
+    /**
+     * Checks if the given method is an autowired setter method.
+     *
+     * @param method The method to be checked
+     * @return True if the method is an autowired setter method, otherwise false
+     */
     public static boolean isAutowiredSetterMethod(Method method) {
         return method.isAnnotationPresent(Autowired.class) && method.getName().startsWith(SET_METHOD_START_PREFIX);
     }
 
+    /**
+     * Retrieves the instance of a class by invoking the default (parameterless) constructor.
+     *
+     * @param clazz The class for which an instance should be created
+     * @return An instance of the specified class
+     * @throws BeanPostProcessorConstructionLimitationException If the default constructor is not present or accessible
+     */
     public static Object getConstructorWithOutParameters(Class<?> clazz) {
         try {
             Constructor<?> constructor = clazz.getConstructor();
@@ -52,6 +73,15 @@ public final class ReflectionUtils {
         }
     }
 
+    /**
+     * Retrieves the instance of a class by invoking a constructor with a single parameter.
+     *
+     * @param clazz         The class for which an instance should be created
+     * @param parameterType The type of the constructor's single parameter
+     * @param instance      The instance to be passed as the constructor's argument
+     * @return An instance of the specified class created using the provided parameter
+     * @throws BeanPostProcessorConstructionLimitationException If the constructor with a single parameter is not present or accessible
+     */
     public static Object getConstructorWithOneParameter(Class<?> clazz, Class<?> parameterType, Object instance) {
         try {
             Constructor<?> constructor = clazz.getConstructor(parameterType);
@@ -64,6 +94,14 @@ public final class ReflectionUtils {
         }
     }
 
+    /**
+     * Retrieves the instance of a class by invoking a constructor with multiple parameters.
+     *
+     * @param clazz                    The class for which an instance should be created
+     * @param parameterTypesToInstance A map containing parameter types and their corresponding instances
+     * @return An instance of the specified class created using the provided parameters
+     * @throws BeanPostProcessorConstructionLimitationException If the constructor with specified parameters is not present or accessible
+     */
     public static Object getConstructorWithParameters(Class<?> clazz, Map<Class<?>, Object> parameterTypesToInstance) {
         try {
             Constructor<?> constructor = clazz.getConstructor(parameterTypesToInstance.keySet().toArray(new Class[0]));
@@ -78,22 +116,53 @@ public final class ReflectionUtils {
         }
     }
 
+    /**
+     * Sets a field's value within an object.
+     *
+     * @param field The field to be modified
+     * @param obj   The object containing the field
+     * @param value The value to be set in the field
+     */
     @SneakyThrows
     public static void setField(Field field, Object obj, Object value) {
         log.trace("Setting into field \"{}\" of {} the value {}", field.getName(), obj, value);
-        field.setAccessible(true);
-        field.set(obj, value);
+        try {
+            field.setAccessible(true);
+            field.set(obj, value);
+        } catch (Exception e) {
+            throw new BringGeneralException(e);
+        }
     }
 
+    /**
+     * Retrieves parameter names of a method or constructor.
+     *
+     * @param methodOrConstructor The method or constructor to retrieve parameter names from
+     * @return A list of parameter names
+     */
     public static List<String> getParameterNames(AccessibleObject methodOrConstructor) {
         return Arrays.stream(info.lookupParameterNames(methodOrConstructor)).toList();
     }
 
+    /**
+     * Extracts the position of a parameter.
+     *
+     * @param parameter The parameter to extract the position from
+     * @return The position of the parameter
+     */
     public static int extractParameterPosition(Parameter parameter) {
         String name = parameter.getName();
         return Integer.parseInt(name.substring(name.indexOf(ARG) + ARG.length()));
     }
 
+    /**
+     * Extracts implementation classes of a generic type.
+     *
+     * @param genericType            The generic type
+     * @param reflections            The Reflections object to query types
+     * @param createdBeanAnnotations List of annotations indicating created beans
+     * @return A list of implementation classes
+     */
     @SneakyThrows
     public static List<Class<?>> extractImplClasses(ParameterizedType genericType, Reflections reflections,
                                                     List<Class<? extends Annotation>> createdBeanAnnotations) {
@@ -112,6 +181,14 @@ public final class ReflectionUtils {
         return Collections.emptyList();
     }
 
+    /**
+     * Extracts implementation classes of a given type.
+     *
+     * @param type                    The type to extract implementations for
+     * @param reflections             The Reflections object to query types
+     * @param createdBeanAnnotations  List of annotations indicating created beans
+     * @return A list of implementation classes
+     */
     public static  List<Class<?>> extractImplClasses(Class<?> type, Reflections reflections,
         List<Class<? extends Annotation>> createdBeanAnnotations) {
         return (List<Class<?>>)reflections.getSubTypesOf(type)
@@ -120,12 +197,28 @@ public final class ReflectionUtils {
             .toList();
     }
 
-    private static boolean isImplementationAnnotated(Class<?> implementation, List<Class<? extends Annotation>> createdBeanAnnotations) {
+    /**
+     * Checks if an implementation class is annotated with any of the specified annotations.
+     *
+     * @param implementation         The implementation class to check
+     * @param createdBeanAnnotations List of annotations indicating created beans
+     * @return True if the implementation is annotated, otherwise false
+     */
+    private static boolean isImplementationAnnotated(Class<?> implementation,
+                                                     List<Class<? extends Annotation>> createdBeanAnnotations) {
         return Arrays.stream(implementation.getAnnotations())
                 .map(Annotation::annotationType)
                 .anyMatch(createdBeanAnnotations::contains);
     }
 
+    /**
+     * Invokes a method on an object and returns a Supplier for its result.
+     *
+     * @param method The method to invoke
+     * @param obj    The object to invoke the method on
+     * @param params The parameters to pass to the method
+     * @return A Supplier representing the method invocation
+     */
     public static Supplier<Object> invokeBeanMethod(Method method, Object obj, Object[] params) {
         return () -> {
             try {
@@ -136,6 +229,15 @@ public final class ReflectionUtils {
         };
     }
 
+    /**
+     * Creates a new instance using the given constructor and arguments and returns a Supplier for it.
+     *
+     * @param constructor The constructor to create the instance
+     * @param args        The arguments to pass to the constructor
+     * @param clazz       The class of the instance to be created
+     * @param proxy       Boolean flag indicating whether to use proxy creation
+     * @return A Supplier representing the new instance creation
+     */
     public static Supplier<Object> createNewInstance(Constructor<?> constructor, Object[] args, Class<?> clazz,
                                                      boolean proxy) {
         return () -> {
@@ -151,9 +253,18 @@ public final class ReflectionUtils {
         };
     }
 
+    /**
+     * Processes the annotations on the methods of a bean.
+     *
+     * @param bean       The bean object
+     * @param declaredMethods The methods of the bean to process
+     * @param annotation The annotation to process
+     * @throws ReflectiveOperationException If an error occurs during reflective operations
+     */
     public static void processBeanPostProcessorAnnotation(Object bean,
                                                           Method[] declaredMethods,
-                                                          Class<? extends Annotation> annotation) throws ReflectiveOperationException {
+                                                          Class<? extends Annotation> annotation)
+            throws ReflectiveOperationException {
         for (Method declaredMethod : declaredMethods) {
             if (declaredMethod.isAnnotationPresent(annotation)) {
                 declaredMethod.invoke(bean);
@@ -161,6 +272,9 @@ public final class ReflectionUtils {
         }
     }
 
+    /**
+     * Inner class extending AnnotationParanamer to handle Qualifier annotations.
+     */
     private static class QualifierAnnotationParanamer extends AnnotationParanamer {
 
         public QualifierAnnotationParanamer(Paranamer fallback) {
